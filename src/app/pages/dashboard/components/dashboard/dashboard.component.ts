4b73e738e2506464a36368889ac359cf12bd01f7
Hau Pham <phaucau2311@gmail.com>
2024-10-16 18:07:16 +0700

    update repo

--- src/app/pages/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/components/dashboard/dashboard.component.ts
@@ -85,8 +85,8 @@ export class DashboardComponent {
 
   converImage(imagePath: any) {
     
-    const baseUrl = 'http://localhost:3000';
-    // const baseUrl = 'https://elearning-be-h3lj.onrender.com'; 
+    // const baseUrl = 'http://localhost:3000';
+    const baseUrl = 'https://elearning-be-h3lj.onrender.com'; 
     // URL cơ sở của bạn
   // Loại bỏ 'uploads' khỏi đường dẫn
   const cleanedImagePath = imagePath.replace('uploads/', ''); 
@@ -298,9 +298,9 @@ export class AppDialogCourseComponent {
   }
 
   converImage(imagePath: any) {
-    const baseUrl = 'http://localhost:3000';
+    // const baseUrl = 'http://localhost:3000';
     let cleanedImagePath = null
-    // const baseUrl = 'https://elearning-be-h3lj.onrender.com'; 
+    const baseUrl = 'https://elearning-be-h3lj.onrender.com'; 
     // URL cơ sở của bạn
     // Loại bỏ 'uploads' khỏi đường dẫn
   if(imagePath.includes('uploads')){
